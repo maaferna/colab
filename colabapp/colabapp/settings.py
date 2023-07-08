b1785d00f3ebc48d77c9886dde01fc3bfadf00a3
ray <enciso.leo@gmail.com>
2023-07-08 16:09:54 +0000

    Index added into views

--- colabapp/colabapp/settings.py
+++ colabapp/colabapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@7#vk8habcb!2vtdfdrc%j4sf-8h7w^@(x=dd*bjvmh037k8j9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'academy.apps.AcademyConfig',
+    'security.apps.SecurityConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'colabapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./colabapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
